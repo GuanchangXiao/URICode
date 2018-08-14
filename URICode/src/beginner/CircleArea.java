1aeb8b137019d30e45e8e38c5a70923ec6227710
GuanchangXiao <822102408@qq.com>
2018-08-14 14:36:08 +0800

    URI

---
+++ URICode/src/beginner/CircleArea.java
@@ -0,0 +1,18 @@
+package beginner;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class CircleArea {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		double r = scanner.nextDouble();
+		double n = 3.14159;
+		double area = n*r*r;
+		DecimalFormat dec = new DecimalFormat("0.0000");
+		String s= dec.format(area);
+		System.out.println("A = "+s);
+	}
+
+}
